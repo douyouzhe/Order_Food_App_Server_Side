d9c9d497f3b5043d27112649db7f093fe6176a44
douyouzhe <douyouzhe@gmail.com>
2018-04-01 23:36:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/youzhedou/order_food_app_server_side/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.youzhedou.order_food_app_server_side;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
